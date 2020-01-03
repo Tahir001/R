f6a3f225c4a058945176b5d21804e2692d6021d8
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2020-01-03 12:32:17 -0500

    Added Hypothesis Testing for One Samples

--- RScripts/Intermediate.R
+++ RScripts/Intermediate.R
@@ -1,7 +1,7 @@
 # Intermediate topics in R.
 # Confidence Intervals & Hypothesis Testing 
 # Note that there is no direct function which calculates just the confidence intervals, hence we will need to create it from scratch for each case.
-# However, we also know that Confidence Intervals are always point_estimate +/- Margin of Error. 
+# However, we also know that Confidence Intervals are always point_estimate +/- Margin of Error.
 
 
 #######################################   CONFIDENCE INTERVALS #######################################
@@ -25,7 +25,24 @@ Lower_tail <- x_bar - qnorm(0.975)*(s/sqrt(n))
 Upper_tail <- x_bar + qnorm(0.975)*(s/sqrt(n))
 # Hence we conclude that we are 95% confident that the true average salary of a data scientist is between 96-104k. 
 
-# One Sample; Small Sample Size 
+# Hypothesis Test: 
+# 1) H0: mu = 113,000 & H1: mu =/= 113,000
+# 2) Calculate your test statistic. 
+t.test(salary_data, mu=113000, alternative="two.sided")
+# 3) Make a decision, then make a real-world conclusion. 
+#Here we can see that since the p-value is below 0.05, we have statistical evidence that the average salary of data scientists is not $113,000.
+
+#Some other cases in R
+# Note that by default, R does a two-sided test. i.e the following is same as above. 
+t.test(salary_data, mu=113000)
+
+# Lower tailed test
+t.test(salary_data, mu=113000, alternative="less")
+
+# Upper tailed test
+t.test(salary_data, mu=113000, alternative="greater")
+
+# One Sample; Small Sample Size
 # Suppose now we had data of n = 9. 
 salary_data2 <- c(78000, 90000,75000,117000, 105000, 96000, 89500, 102300, 80000)
 # Find the appropriate statistic, taking into consideration the degrees of freedom (if applicable) for 99% confidence
@@ -47,6 +64,19 @@ Upper_tail
 # Hence we can be 99% confident that the mean salary for a data scientist is between $76,950 to $108,115. 
 # Notice how the interval got bigger as we increased the confidence level. 
 
+# Hypothesis Test
+
+# Question: Estimate if the email open rate of one of our firm's competitor is above your company's. 
+# Open rate data for a competitior firm
+open_rate_data <- c(0.26,0.23,0.42,0.49,0.23,0.59,0.29,0.29,0.57,0.40)
+
+# Test at a significance level of 0.05. (Assume it is 0.05 when a significance level is not given)
+# 1) H0: mu <= 0.40 & H1: mu > 0.40
+# 2) Calculate your test statistic
+# The Student's T distribution works here because of small small, and variance unknown. 
+t.test(open_rate_data, mu = 0.40, alternative="greater")
+# 3) Make a decision: Since Pvalue > alpha, we FTR H0. 
+
 # Two Samples; Dependent --> Paired Data, Positive Co-relation, pairs are independent. 
 # Usually use this test for when researching same subject over time -> Before & After situations, positive co-relation.
 # Or when looking at cause and effect situations 
@@ -55,6 +85,39 @@ difference = before - after
 dbar = mean(difference)
 # Construct CI like above, i.e:
 CI <- dbar +/- (z_alpha/2 or t_n-1;alpha/2) * (std/sqrt(n))
+
+# Hypothesis Testing
+# Test to see if a drug companies claims are true about thier new drug increasing magnesium levels in patients. 
+
+# Getting our data
+before <- c(2,1.4,1.3,1.1,1.8,1.6,1.5,0.7,0.9,1.5)
+after <- c(1.7,1.7,1.8,1.3,1.7,1.5,1.6,1.7,1.7,2.4)
+
+# Check to see if our data is co-related
+cor(before,after)
+plot(before,after)
+
+# H0: mu_d >= 0 & H1: mu_d < 0
+
+# Two samples, paired t-test.
+t.test(before,after,paired=T)
+
+# Here we can conlcude that at 5% significance level, we have sufficient evidence that the company's new drug does indeed increase patients magnesium levels. 
+
+# Lets construct a CI for this example aswell. 
+difference <- c( before - after)
+
+# Calculate the standard error
+dbar <- mean(difference)
+s <- sd(difference)
+n <- length(difference)
+SE <- s/(sqrt(n))
+
+# The 95% CI
+lower_tail <- dbar - qt(0.975,9)*SE
+upper_tail <- dbar + qt(0.975,9)*SE
+
+# As we can see, this matches our CI which we got from the built-in t.test command! Nice =). 
 
 #Two Samples; Independent. Case I) Population variance is known
 # Calculate the 99% confidence interval for the difference of two means. 
@@ -116,9 +179,14 @@ Upper_tail <- (xbar - ybar) + (t_value*SE)
 
 #######################################  HYPOTHESIS TESTING ####################################### 
 
-# Recall that a Hypothesis test Always concludes 4 steps: 1) Determine H_0, H_a, 2) Find Test Statistic, 3) Determine the p-value and 4) Conclude. 
-# Lets follow the steps to do different cases in R.
-# Hypothesis Testing;  I) One mean Large Samples
+# Let's shift our focus on Hypothesis testing. The different cases of Hypothesis testing (situations where they are applicable), is the
+# same as thier counter-part confidence Intervals. Hence, the cases we will be covering are: 
+# Single Populaion/One Samples: Large & Small Sample Size for One mean
+# Multiple Populations/ Two Samples: Dependent Samples, and Independent Samples (I) Variance Known, (II) Variance unkown but assumed equal
+# and (III) Variance unknown and not equal. Lets start with our first case. 
+
+# One Sample; Large Sample Size 
+# Lets get some data
 
 
 
