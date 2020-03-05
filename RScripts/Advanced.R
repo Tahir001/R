ebd998c697d3308c3dbaa3d6a2245af315223b19
Tahir M <tahir.muhammad@mail.utoronto.ca>
2020-03-04 20:53:02 -0500

    added convulution method

--- RScripts/Advanced.R
+++ RScripts/Advanced.R
@@ -1,5 +1,10 @@
 # Tahir Muhammad | University of Toronto
-# Advanced concepts in R: Stimulation, Inverse Transfomation Method, Monte Carlo Stimulations, and the Apply family of functions. 
+# Advanced concepts in R: Simulation, Inverse Transformation Method, Acceptance Rejection Method, Monte Carlo Estimation & Transformations
+#    Stimulation of Random Numbers: How do we simulate the uniform distribution
+#    How can we simulate any distribution? Two methods: 
+#    I) Inverse Transfomation Method & II) Acceptance-Rejection Method
+#    Transformation Methods: Convulution, Mixtures, Box-Mueller Transformation
+#    Solving any integration problems: Monte Carlo Estimations 
 
 ####################################
 #### Stimulating random numbers ####
@@ -41,7 +46,7 @@ x <- runif(n=10000,min=0,max=1)
 hist(x)
 
 ########################################
-#### Inverse Transformation Method ####
+#### Inverse Transformation Method #####
 ########################################
 # Given a density function, can generate it's  distribution. 
 # How? For Continous Distributions:
@@ -59,6 +64,28 @@ y <- seq(0, 1, .01)
 lines(y, 3*y^2, col="black") 
 
 # Inverse Value Method; Discrete Case: Recursive
+
+#########################################
+######## Mixtures & Convulutions ########
+#########################################
+# Convulution is a transformation method which allows you to find the distribution of the sum of two random variables.
+# Example: Adding hieght of class 1 + hieght of class 2. (They must come from the same distribution)
+# Some common distributions are convolutions! For example, Binomial is a sum of bernoulli -> convulution. Exponential -> Gamma, .. etc.
+# Mixtures is a derived density from one or more distributions. Ex: Adding male distribution to female distribution
+# The sum of the wieghts must add up to 1. Ex: 05Fx_1(x) + 0.3Fx_2(x) + 0.2F_x3(x) => 0.5+0.2 +0.3 = 1. 
+
+# Convulution Example: What is the density of S, where S = X1 + X2 + X3, and X1 ~ N(0,1), X2 ~ N(11,20), & X3 ~ N(-1,1) ?
+n <- 10000
+x1 <- rnorm(n,0,1)
+x2 <- rnorm(n,11,20)
+x3 <- rnorm(n,-1,1)
+S <- x1 + x2 + x3
+hist(S) # We can see the resulting density is also normal. 
+
+# Mixtures Example: 
+# To simulate mixtures, we apply the composition technique. It is briefly summarized below.
+# 
+
 
 # Monte Carlo Stimulation
 
