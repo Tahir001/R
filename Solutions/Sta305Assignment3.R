8d0c39fd0399623d61f9cb4157028d31f1b3b86c
Tahir M <tahir.muhammad@mail.utoronto.ca>
2020-03-21 19:24:39 -0400

    Uploaded Assignment3 Sta305 Solutions Q1

---
+++ Solutions/Sta305Assignment3.R
@@ -0,0 +1,19 @@
+# STA305 Assignment 3 | University of Toronto
+
+###################################
+#### Question 2c, Assignment 2 ####
+####      Muhammad Tahir       ####
+####        1002537613         ####
+###################################
+
+# Set the directory to the data folder
+setwd("/home/tahir/Downloads/")
+
+# Read in the data
+data = read.csv("Problem2.csv",header = TRUE)
+
+
+
+
+
+
