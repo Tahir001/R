e0b338093b2af6675762ff09f568b38750ea4c03
Tahir M <tahir.muhammad@mail.utoronto.ca>
2020-10-26 08:59:16 -0400

    Include Data Cleaning

--- RScripts/Basics-II.R
+++ RScripts/Basics-II.R
@@ -1,2 +1,12 @@
 # Tahir Muhammad | University of Toronto
-# Basics II: All about plotting, and graphs!
+# Basics II: Data analysis and visualization
+# Introduction to Statistical Reasoning and Data Science -- STA130.
+# Full course Implemented in R with Examples, and used for the purpose of teaching others. 
+
+# Installing a package
+#install.packages("tidyverse")
+
+# Loading a package
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
